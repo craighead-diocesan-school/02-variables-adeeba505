7d0f46f7347b836cb5bb9629fa5e202e062c6c9f
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-06-07 22:56:19 +0000

    finished the test life remaining task

--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,9 @@
 // ####################################
 
 function lifeRemaining() {
-  
+  alert("The average life expectancy is 83 years")
+  let age = prompt("How old are you?")
+  let lifetime = 83
+  let lifeleft = lifetime - age
+  alert("You've got " + lifeleft +' left in you')
 }
