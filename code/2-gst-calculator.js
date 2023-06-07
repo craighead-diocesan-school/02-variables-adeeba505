740d7e8a6d6ea1b62396af6f8407194ac4116081
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-06-07 22:35:35 +0000

    finished the gst calculator one

--- code/2-gst-calculator.js
+++ code/2-gst-calculator.js
@@ -3,5 +3,10 @@
 // ####################################
 
 function gstCalculator() {
-  alert('The price with GST the price and 15%.')
+  alert('The price with GST is the price and 15%.')
+  
+  let price = prompt('What is the cost before GST?')
+
+  alert('THe cost with GST is ' + price*1.15)
+
 }
