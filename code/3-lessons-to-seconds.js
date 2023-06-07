519566343d16525f5f3119d6dda49aaeb7bd7d93
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-06-07 22:42:47 +0000

    finished the lessons to seconds task

--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,6 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+  let lessons = prompt('How many lessons do you have left today?')
+  alert("That's "+lessons*3600+' seconds')
 }
