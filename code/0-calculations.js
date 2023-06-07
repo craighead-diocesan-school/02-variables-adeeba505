0aaf8939c2b64cda4c49620fcc38d24f4aeffd47
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-06-07 22:23:35 +0000

    Just did the pupper age coding

--- code/0-calculations.js
+++ code/0-calculations.js
@@ -3,3 +3,22 @@
 // ####################################
 
 function calculations() {}
+alert(8 + 2)
+alert(8 - 2)
+alert(8 * 2)
+alert(10 / 4)
+
+let firstNumber = 20
+let secondNumber = 5
+
+let result = firstNumber * secondNumber
+
+alert(result)
+
+firstNumber = prompt("Enter a number")
+secondNumber = 2
+
+result = first_number * second_number
+
+alert((firstNumber = " x " + " = "), result)
+
